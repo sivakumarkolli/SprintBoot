bf6356c7a3515df838948fbd261a71633b53f3ba
Siva Kumar Kolli <sivakumarkolli@Sivas-MacBook-Pro.local>
2021-02-10 18:49:01 -0500

    my first git project

---
+++ FirstSample/src/main/java/com/cva/FirstSample/FirstSampleApplication.java
@@ -0,0 +1,13 @@
+package com.cva.FirstSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSampleApplication.class, args);
+		System.out.println("This is the first program for GIT");
+	}
+}
